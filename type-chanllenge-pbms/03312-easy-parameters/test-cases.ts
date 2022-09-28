954d3046607a769eaf4af16fede28327ae7299b4
Zourunfa <51783403+Zourunfa@users.noreply.github.com>
2022-09-28 11:16:40 +0800

    feat:add 3 ti

---
+++ type-chanllenge-pbms/03312-easy-parameters/test-cases.ts
@@ -0,0 +1,9 @@
+// const foo = (arg1: string, arg2: number): void => {}
+
+// type FunctionParamsType = MyParameters<typeof foo> // [arg1: string, arg2: number]
+
+type MyParameters<T extends (...args: any[]) => any> = T extends (
+  ...args: infer R
+) => any
+  ? R
+  : never;
